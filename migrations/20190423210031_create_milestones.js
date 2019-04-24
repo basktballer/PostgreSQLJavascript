b9c9de36bf223f3fabc7d98a46364d055aeb117b
Eden Yeung <basktballer@gmail.com>
2019-04-24 00:05:48 +0000

    Create milestones

--- migrations/20190423210031_create_milestones.js
+++ migrations/20190423210031_create_milestones.js
@@ -2,9 +2,9 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('milestones', function(table){
-      table.increments('id');
-      table.string('description');
-      table.date('date_achieved');
+      table.increments('id')
+      table.string('description')
+      table.date('date_achieved')
     })
   ])
 };
@@ -12,8 +12,8 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('milestones', function(table){
-      table.dropColumn('id');
-      table.dropColumn('description');
+      table.dropColumn('id')
+      table.dropColumn('description')
       table.dropColumn('date_achieved')
     })
   ])
